ef7b3e42012753323967628b7b6bdfa8510cafaa
Th4nkY0ux3 <131253874+Th4nkY0ux3@users.noreply.github.com>
2023-09-11 20:44:32 +0200

    Add files via upload

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,23 @@
 /*   By: seckhard <seckhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 14:30:54 by seckhard          #+#    #+#             */
-/*   Updated: 2023/09/11 15:10:05 by seckhard         ###   ########.fr       */
+/*   Updated: 2023/09/11 20:43:39 by seckhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft.h>
+
+int ft_memcmp(const void *ptr1, const void *ptr2, size_t size)
+{
+    const unsigned char *p1 = ptr1;
+    const unsigned char * p2 = ptr2;
+    size_t i = 0;
+
+    while (i < size)
+    {
+        if (p1[i] != p2[i])
+            return(int)(p1[i] - p2[i]);
+        i++;
+    }
+    return(0);
+}
